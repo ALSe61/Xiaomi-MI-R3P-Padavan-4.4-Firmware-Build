3c1c03786cb03001e49adab3d888c7105333da68
ALSe61 <alse61@gmail.com>
2024-09-07 23:40:34 +0500

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -2,13 +2,13 @@
 
 user_name="root"                                    # 用户名
 user_password="root"                                # 登录密码
-lan_ip="192.168.31"                                 # LAN 地址 别写后面的 .1
-wlan_2g_ssid="int_2.4G"                             # 2G 无线名称
-wlan_5g_ssid="int_5G"                               # 5G 无线名称
-wlan_guest_2g_ssid="int_pub_2.4G"                   # 2G 访客无线名称
-wlan_guest_5g_ssid="int_pub_5G"                     # 5G 访客无线名称
-wlan_2g_psk="qwerasdf"                              # 2G WIFI密码最少8位 空白为不设置
-wlan_5g_psk="qwerasdf"                              # 5G WIFI密码最少8位 空白为不设置
+lan_ip="192.168.1"                                 # LAN 地址 别写后面的 .1
+wlan_2g_ssid="MiR3P"                             # 2G 无线名称
+wlan_5g_ssid="MiR3P"                               # 5G 无线名称
+#wlan_guest_2g_ssid="int_pub_2.4G"                   # 2G 访客无线名称
+#wlan_guest_5g_ssid="int_pub_5G"                     # 5G 访客无线名称
+wlan_2g_psk="123456789"                              # 2G WIFI密码最少8位 空白为不设置
+wlan_5g_psk="123456789"                              # 5G WIFI密码最少8位 空白为不设置
 version_time=$(date +%Y%m%d)                        # 更新时版本号时间: 20210101
 default_file="./user/shared/defaults.h"             # 默认配置文件
 
@@ -28,16 +28,16 @@ echo "修改 5G 无线名称"
 sed -i 's/DEF_WLAN_5G_SSID	BOARD_PID "_5G_%s"/DEF_WLAN_5G_SSID	"'$wlan_5g_ssid'"/g' $default_file
 
 echo "修改 2G 访客无线名称"
-sed -i 's/DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"/DEF_WLAN_2G_GSSID	"'$wlan_guest_2g_ssid'"/g' $default_file
+#sed -i 's/DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"/DEF_WLAN_2G_GSSID	"'$wlan_guest_2g_ssid'"/g' $default_file
 
 echo "修改 5G 访客无线名称"
-sed -i 's/DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"/DEF_WLAN_5G_GSSID	"'$wlan_guest_5g_ssid'"/g' $default_file
+#sed -i 's/DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"/DEF_WLAN_5G_GSSID	"'$wlan_guest_5g_ssid'"/g' $default_file
 
 echo "修改 2.4GHz WIFI 密码"
-sed -i 's/DEF_WLAN_2G_PSK		"1234567890"/DEF_WLAN_2G_PSK		"'$wlan_2g_psk'"/g' $default_file
+sed -i 's/DEF_WLAN_2G_PSK		"123456789"/DEF_WLAN_2G_PSK		"'$wlan_2g_psk'"/g' $default_file
 
 echo "修改 5GHz WIFI 密码"
-sed -i 's/DEF_WLAN_5G_PSK		"1234567890"/DEF_WLAN_5G_PSK		"'$wlan_5g_psk'"/g' $default_file
+sed -i 's/DEF_WLAN_5G_PSK		"123456789"/DEF_WLAN_5G_PSK		"'$wlan_5g_psk'"/g' $default_file
 
 echo "更新版本号时间"
 sed -i "s/FIRMWARE_BUILDS_REV=.*/FIRMWARE_BUILDS_REV=$version_time/g" ./versions.inc
